0706074a760b85c32f5a13bf0c59285ee4854792
jianhuashao <jianhua.shao1986@gmail.com>
2013-09-04 19:29:53 +0100

    parse app done

--- parse_app_review.py
+++ parse_app_review.py
@@ -1,5 +1,21 @@
 import parse_app
 import os
+import my
+db_path = './amazon_parse_html.db'
+import sqlite3
+db = sqlite3.connect(db_path)
+from bs4 import BeautifulSoup, Comment
+import codecs
+import re
+from datetime import datetime
+
+def parse_file_app_review(p):
+	print p
+	soup = BeautifulSoup(open(p).read())
+	comments = soup.find_all(text=lambda text:isinstance(text, Comment))
+	for comment in comments:
+		print comment
+
 
 def loop_dir(p):
 	i = 0
@@ -13,6 +29,7 @@ def loop_dir(p):
 			parse_file_app_review(realpath)
 			i = i + 1
 			print i, realpath
+			return
 		print "=="
 
 if __name__ == '__main__':
