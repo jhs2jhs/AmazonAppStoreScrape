9acbebf187f60f46121ecd461abbc6dd18b5a091
jianhuashao <jianhua.shao1986@gmail.com>
2013-09-17 14:32:18 +0100

    parse_app_category

--- parse_app_category.py
+++ parse_app_category.py
@@ -38,6 +38,7 @@ def parse_file_app_category(p, category, game_sub):
 	return len(asins)
 
 
+# html folder: it is initially used by macos
 def loop_dir_1(p):
 	dir_lists = os.listdir(p)
 	t = len(dir_lists)
@@ -77,7 +78,45 @@ def loop_dir_1(p):
 		print "====", dir_list, t, category, game_sub
 
 
+## html2 folder: it is used by DTC. This is the default one
+def loop_dir_2(p):
+	dir_lists = os.listdir(p)
+	t = len(dir_lists)
+	k = 0
+	for dir_list in dir_lists:
+		#dir_list = 'books & comics_2478833011'
+		dir_fullpath = os.path.join(p, dir_list)
+		t = 0
+		if not os.path.isdir(dir_fullpath):
+			print '**********is not dir', dir_fullpath
+			continue
+		f_lists = os.listdir(dir_fullpath)
+		category = dir_list.split("_")[0].strip().lower()
+		game_sub = ''
+		print "==== start ", dir_list, t, category, game_sub
+		i = 0
+		j = len(f_lists)
+		for f_list in f_lists:
+			#print f_list
+			f_fullpath = os.path.join(dir_fullpath, f_list)
+			if not os.path.isfile(f_fullpath):
+				print '**********is not file', f_fullpath
+				continue
+			realpath = os.path.realpath(f_fullpath)
+			#print i, j, k, t, realpath, str(datetime.now())
+			fileName, fileExtension = os.path.splitext(realpath)
+			if fileExtension != '.html':
+				continue
+			t = t + parse_file_app_category(realpath, category, game_sub)
+			#print t
+			#return
+			i = i + 1
+		k = k + 1
+		print "==== end ", dir_list, t, category, game_sub
+
+
 if __name__ == '__main__':
-	loop_dir('./html/category')
+	#loop_dir_1('./html/category')
+	loop_dir_2('./html2/category')
 	#print parse_file_app_category('/Users/jianhuashao/github/AmazonAppStoreScrape/html/category/books & comics_2478833011/http___www.amazon.com_b_ref=sr_pg_522?ie=UTF8&node=2478833011&page=522.html', 'book')
 	#parse_file_app_review('/Users/jianhuashao/github/AmazonAppStoreScrape/html_review/B004GJ6BY0/4.html', 'asin')
